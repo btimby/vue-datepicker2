dc28c28ed61237d22321832a7518d8dc20c64604
Ben Timby <btimby@gmail.com>
2020-12-14 10:52:57 -0500

    First pass.

---
+++ vue-datepicker2.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="input-append date ac-start-date-text">
+    <input v-bind="value" type="text">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VueDatepicker2',
+
+  props: {
+    value: null,
+
+    options: {
+      clearBtn: true,
+      autoclose: true,
+      todayHighlight: true,
+    }
+  },
+
+  mounted() {
+    $(this.$el).datepicker(this.options);
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
